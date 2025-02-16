3dc7a7a74a517f20d63c120832a035e44143cd35
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-02-16 11:37:39 -0800

    Removed unneeded imports and values

--- packages/react-frontend/src/routes/write/WritePage.tsx
+++ packages/react-frontend/src/routes/write/WritePage.tsx
@@ -1,12 +1,11 @@
 import {Marked} from "marked";
-import {ChangeEvent, Fragment, KeyboardEvent, useRef, useState} from "react";
+import {Fragment, KeyboardEvent, useRef, useState} from "react";
 import { markedHighlight } from "marked-highlight";
 import hljs from "highlight.js";
 import "./WritePage.css";
 import {useEditable} from "use-editable";
+import {SaveIcon} from "../../assets/icons";
 
-const SPLITTER = /\r?\n/;
-const exampleMd = "# Title\n\n## Contents\n\n## What is a penguin?\nA penguin is a thing that does *not* exist.\n\n### What about those birds?\n**Not real**\n\n## What is an Elephat\nBest animal ever :)\n"
 
 export default function WritePage() {
     const marked = new Marked(
@@ -46,11 +45,30 @@ export default function WritePage() {
         }
     }
 
+    const handleSubmit = () => {
+        const { text } = editorHandler.getState();
+
+        uploadEntry(text);
+    }
+
     return (
         <div className="p-6 gap-6 flex flex-col bg-primary-600 h-full w-full">
-            <h1>Write your Entry!</h1>
+            {/* Toolbar */}
+            <div className="flex flex-row">
+                <div className="flex justify-center items-center flex-1">
+                    <h1>Write your Entry!</h1>
+                </div>
+                <div className="flex justify-center items-center flex-1">
+                    <button
+                        className="btn"
+                        onClick={handleSubmit}>
+                        <SaveIcon className="icon-xs"/>
+                        Submit
+                    </button>
+                </div>
+            </div>
 
-            <div className="flex flex-row justify-center items-center h-full w-full">
+            <div className="bg-primary-900 py-4 flex flex-row justify-center items-center h-full w-full">
                 {/* Input */}
                 <div
                     id="md-editor"
@@ -59,7 +77,7 @@ export default function WritePage() {
                     onKeyDown={handleKeyPress}
                     contentEditable={true}
                     suppressContentEditableWarning={true}
-                    className="p-6 bg-primary-900 h-full w-full flex-1">
+                    className="py-2 px-6 h-full w-full md:border-r-2 border-secondary-400 flex-1">
                     {text.split(/\r?\n/).map((content, i, arr) => (
                         <Fragment key={i}>
                             {/* Split identifiers from text */}
@@ -76,8 +94,12 @@ export default function WritePage() {
                 </div>
 
                 {/* Preview */}
-                <div className="content p-6 bg-primary-900 h-full w-full flex-1" dangerouslySetInnerHTML={{__html: html}}/>
+                <div className="content py-2 px-6 h-full w-full flex-1" dangerouslySetInnerHTML={{__html: html}}/>
             </div>
         </div>
     );
+}
+
+function uploadEntry(entry: string) {
+    console.log(entry);
 }
