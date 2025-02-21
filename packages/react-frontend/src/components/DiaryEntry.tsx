505efbad60ff83df7390bf7c5e6738e69be16cef
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-02-21 09:30:47 -0800

    Moved diary entry to new component

---
+++ packages/react-frontend/src/components/DiaryEntry.tsx
@@ -0,0 +1,17 @@
+export interface DiaryEntryProps {
+    title: string,
+    date: string,
+    body: string
+}
+
+export default function DiaryEntry({title, date, body}: DiaryEntryProps ) {
+    return (
+        <div className="border border-accent-700 rounded-2xl p-6 shadow-lg bg-primary-100 text-white relative"
+             style={{minHeight: "150px"}}
+        >
+            <h2 className="text-xl font-bold text-accent-300">{title}</h2>
+            <p className="text-sm text-secondary-200">{date}</p>
+            <p className={"mt-3 text-secondary-50"}>{body}</p>
+        </div>
+    )
+}
