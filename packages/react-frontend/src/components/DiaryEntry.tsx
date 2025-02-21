bfdcef46788bb859640f23df6d64af7c6558ec0c
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-02-21 09:31:15 -0800

    Moved minHeight to tailwind

--- packages/react-frontend/src/components/DiaryEntry.tsx
+++ packages/react-frontend/src/components/DiaryEntry.tsx
@@ -6,9 +6,7 @@ export interface DiaryEntryProps {
 
 export default function DiaryEntry({title, date, body}: DiaryEntryProps ) {
     return (
-        <div className="border border-accent-700 rounded-2xl p-6 shadow-lg bg-primary-100 text-white relative"
-             style={{minHeight: "150px"}}
-        >
+        <div className="border border-accent-700 rounded-2xl p-6 shadow-lg bg-primary-100 text-white relative min-h-[150px]" >
             <h2 className="text-xl font-bold text-accent-300">{title}</h2>
             <p className="text-sm text-secondary-200">{date}</p>
             <p className={"mt-3 text-secondary-50"}>{body}</p>
