9e5f2187e47628e777a459062fb026b3304b8f7f
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-02-21 09:59:48 -0800

    Added link to icons collection

--- packages/react-frontend/src/assets/icons.tsx
+++ packages/react-frontend/src/assets/icons.tsx
@@ -1,5 +1,8 @@
 import {SVGProps} from "react";
 
+// All icons from this collection:
+// https://www.svgrepo.com/collection/solar-bold-icons/
+
 export function HomeIcon(props: SVGProps<SVGSVGElement>) {
     return (
         <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
