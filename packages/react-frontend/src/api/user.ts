602769d95f21d5945a19e2f238b8156d62c058a5
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-02-24 09:17:35 -0800

    Basic user

--- packages/react-frontend/src/api/user.ts
+++ packages/react-frontend/src/api/user.ts
@@ -3,6 +3,28 @@ const user = {
     email: "DiaryUser1085@gmail.com",
     password: "fake.password1234!!",
     diaries: [
-
+        {
+            title: "Diary 1",
+            date: "12-01-2025",
+            entries: [
+                {
+                    title: "Dear diary",
+                    data: "",
+                    body: ""
+                }
+            ]
+        },
+        {
+            title: "A Second Diary",
+            date: "12-01-2025"
+        },
+        {
+            title: "Auguest",
+            date: "12-01-2025"
+        },
+        {
+            title: "Hi there :)",
+            date: "12-01-2025"
+        }
     ]
 }
