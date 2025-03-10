d92244c30840af5b12580182c22132c52b77e2cb
calebhuang <calebhuang0206@gmail.com>
2025-03-10 10:47:20 -0700

    change password feature

--- packages/react-frontend/src/routes/account/AccountPage.tsx
+++ packages/react-frontend/src/routes/account/AccountPage.tsx
@@ -3,8 +3,12 @@ import {Link} from "react-router-dom";
 import {UserCircleIcon} from "../../assets/icons";
 
 export default function AccountsPage() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false);
+    const [password, setPassword] = useState("");
+
+    const [isPicModalOpen, setIsPicModalOpen] = useState(false);
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
     const [preview, setPreview] = useState<string | null>(null);
     const [profilePicture, setProfilePicture] = useState<string | null>(null);
     const fileInputRef = useRef<HTMLInputElement>(null);
@@ -16,19 +20,37 @@ export default function AccountsPage() {
             setPreview(URL.createObjectURL(file));
         }
     };
+    const handlePasswordChange = (event) => {
+        const newPassword = event.target.value;
+        if (newPassword) {
+            setPassword(newPassword);
+        }
+    };
+    const handleNewPassword = () => {
+        if (password) {
+            alert("Password changed successfully"); // replace later
+            setIsPasswordModalOpen(false);
+            setPassword(""); // clear
+        }
+
+    }
+    const handleCancelPassword = () => {
+        setIsPasswordModalOpen(false);
+        setPassword("");
+    };
     const handleUpload = () => {
         if (selectedFile) {
             console.log("uploading: ", selectedFile);
             alert("Profile picture updated!"); // replace later with actual logic
 
             setProfilePicture(preview);
-            setIsModalOpen(false);
+            setIsPicModalOpen(false);
             setSelectedFile(null);
             setPreview(null);
         }
     };
-    const handleCancel = () => {
-        setIsModalOpen(false);
+    const handleCancelUpload = () => {
+        setIsPicModalOpen(false);
         setSelectedFile(null);
         setPreview(null);
     };
@@ -36,7 +58,7 @@ export default function AccountsPage() {
     const triggerFileInput = () => {
         fileInputRef.current?.click();
     };
-
+    // const el = document.getElementById();
     return (
         <div className="flex flex-col items-center gap-6 p-12 min-h-screen">
             <h1 className="text-2xl font-bold ">Account Settings</h1>
@@ -54,14 +76,14 @@ export default function AccountsPage() {
                     </div>
                 )}
                 {/*<UserCircleIcon className="icon-lg"/>*/}
-                <button onClick={() => setIsModalOpen(true)}
+                <button onClick={() => setIsPicModalOpen(true)}
                         className="mt-2 p-2 rounded-lg text-accent-200 hover:underline"
                 >
                     Change Profile Picture
                 </button>
-                <a href="#" className="mt-2 text-accent-200 hover:underline">
+                <button onClick={() => setIsPasswordModalOpen(true)}>
                     Reset Password
-                </a>
+                </button>
             </div>
 
             <Link
@@ -71,8 +93,8 @@ export default function AccountsPage() {
                 Log Out
             </Link>
 
-            {isModalOpen && (
-                <div className="fixed inset-0 flex items-center justify-center ">
+            {isPicModalOpen && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
                     <div className="flex flex-col bg-white items-center justify-center p-6 gap-5 rounded-lg w-96">
                         {/* File input*/}
                         <input type="file"
@@ -97,7 +119,7 @@ export default function AccountsPage() {
 
                         {/* Buttons */}
                         <div className="flex justify-end gap-2">
-                            <button onClick={handleCancel}
+                            <button onClick={handleCancelUpload}
                                     className="px-4 py-2 bg-accent-50 text-black rounded-lg shadow hover:bg-accent-100 transition"
                             >
                                 Cancel
@@ -114,8 +136,40 @@ export default function AccountsPage() {
                     </div>
                 </div>
             )}
+
+            {isPasswordModalOpen && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+                    <div className="flex flex-col bg-white items-center justify-center p-6 gap-5 rounded-lg w-96">
+                        {/* File input*/}
+                        <input type="text"
+                               placeholder="Enter new password"
+                               value={password}
+                               onChange={handlePasswordChange}
+                               className="w-80 h-10 object-cover text-gray-800 rounded-2xl shadow focus:ring-2 focus:ring-accent-500 outline-none transition"
+                        />
+
+
+                        {/* Buttons */}
+                        <div className="flex justify-end gap-2">
+                            <button onClick={handleCancelPassword}
+                                    className="px-4 py-2 bg-accent-50 text-black rounded-lg shadow hover:bg-accent-100 transition"
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                onClick={handleNewPassword}
+                                disabled={!password}
+                                className={`px-4 py-2 text-white ${
+                                    password ? "bg-accent-500 hover:bg-accent-400" : "bg-gray-400 cursor-not-allowed"
+                                }`}
+                            >
+                                Change Password
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-        // Modal
 
 
     );
