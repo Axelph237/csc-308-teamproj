bda3ea168cde0b7b79606765fd996b76df64b39d
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-03-12 09:48:24 -0700

    Updated markdown tests

--- packages/react-frontend/__tests__/components/Markdown.test.tsx
+++ packages/react-frontend/__tests__/components/Markdown.test.tsx
@@ -1,18 +1,20 @@
-import {expect, test} from '@jest/globals';
+import {describe, expect, it} from '@jest/globals';
 import {render, screen} from '@testing-library/react';
 import Markdown from "../../src/components/Markdown";
 
 const testSource = "# Heading\n## Subheading";
 
-test('Test correct markdown parsing', async () => {
-    // Render the Markdown component
-    const {debug} = render(<Markdown source={testSource}/>);
+describe("Markdown Component", () => {
+    it("Test Markdown parser", async () => {
+        // Render the Markdown component
+        const {debug} = render(<Markdown source={testSource}/>);
 
-    // Test heading
-    const heading = await screen.findByText("Heading");
-    expect(heading.tagName).toBe("H1");
+        // Test heading
+        const heading = await screen.findByText("Heading");
+        expect(heading.tagName).toBe("H1");
 
-    // Test subheading
-    const subheading = await screen.findByText("Subheading");
-    expect(subheading.tagName).toBe("H2");
-});
+        // Test subheading
+        const subheading = await screen.findByText("Subheading");
+        expect(subheading.tagName).toBe("H2");
+    });
+})
