9b385da8a60e1d2d45b165f68562325bbb671abe
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-03-12 10:12:55 -0700

    Removed unused imports

--- packages/react-frontend/__tests__/routes/home/HomePage.test.tsx
+++ packages/react-frontend/__tests__/routes/home/HomePage.test.tsx
@@ -1,12 +1,7 @@
 import {render, screen, waitFor} from "@testing-library/react";
 import {MemoryRouter} from "react-router-dom";
 import HomePage from "../../../src/routes/home/HomePage";
-import {getUserDiaries} from "../../../src/api/user";
-import {expect, describe, it, jest} from "@jest/globals";
-//import '@testing-library/jest-dom';
-
-// const {expect, describe, it} = require('@jest/globals');
-// Mock the API function
+import {expect, describe, it} from "@jest/globals";
 
 describe("HomePage Component", () => {
     it("renders the HomeHeader component", async () => {
