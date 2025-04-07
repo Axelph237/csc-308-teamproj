644c350bac28a638196fd5fcbec0200cac563df9
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-04-07 11:56:19 -0700

    Added docs

--- packages/types/user.ts
+++ packages/types/user.ts
@@ -1,6 +1,8 @@
 import {Type} from "@sinclair/typebox";
 import {objectIdObj} from "./objectId";
 
+// https://github.com/sinclairzx81/typebox?tab=readme-ov-file#typeregistry-format
+
 const user = Type.Object({
     _id: objectIdObj,
     username: Type.String()
