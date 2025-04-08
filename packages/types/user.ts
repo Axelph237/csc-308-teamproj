4ce6db338aa564a4cde2c5cb1fdc1f84755cbee3
calebhuang <calebhuang0206@gmail.com>
2025-04-07 23:40:35 -0700

    user, page, diary type objects created

--- packages/types/user.ts
+++ packages/types/user.ts
@@ -5,7 +5,26 @@ import {objectIdObj} from "./objectId";
 
 const user = Type.Object({
     _id: objectIdObj,
-    username: Type.String()
+    username: Type.String(),
+    password: Type.String(),
+    email: Type.String(),
+    diariesID: Type.Array(objectIdObj),
+    profilePicture: Type.String()
+})
+
+const page = Type.Object({
+    _id: objectIdObj,
+    title: Type.String(),
+    date: Type.String(),
+    body: Type.String(),
+})
+
+const diary = Type.Object({
+    _id: objectIdObj,
+    title: Type.String(),
+    lastEntry: Type.String(),
+    numEntries: Type.Integer(),
+    entries: Type.Array(page),
 })
 
 //Type.Optional(Type.String()),
