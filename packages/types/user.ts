e413895ad0d4badd9e67858c44b1b8d3625e43fe
calebhuang <calebhuang0206@gmail.com>
2025-04-09 11:17:35 -0700

    separated types to different files

--- packages/types/user.ts
+++ packages/types/user.ts
@@ -1,30 +1,11 @@
 import {Type} from "@sinclair/typebox";
 import {objectIdObj} from "./objectId";
 
-// https://github.com/sinclairzx81/typebox?tab=readme-ov-file#typeregistry-format
-
-const user = Type.Object({
+export const user = Type.Object({
     _id: objectIdObj,
     username: Type.String(),
     password: Type.String(),
     email: Type.String(),
     diariesID: Type.Array(objectIdObj),
-    profilePicture: Type.String()
+    profilePicture: Type.String({format: "uri"})
 })
-
-const page = Type.Object({
-    _id: objectIdObj,
-    title: Type.String(),
-    date: Type.String(),
-    body: Type.String(),
-})
-
-const diary = Type.Object({
-    _id: objectIdObj,
-    title: Type.String(),
-    lastEntry: Type.String(),
-    numEntries: Type.Integer(),
-    entries: Type.Array(page),
-})
-
-//Type.Optional(Type.String()),
