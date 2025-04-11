952d9196bfc493d49bd87ac1adc5caafc5dc47af
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-04-11 11:44:51 -0700

    Created WritePage tests

--- packages/react-frontend/__tests__/routes/write/WritePage.test.tsx
+++ packages/react-frontend/__tests__/routes/write/WritePage.test.tsx
@@ -1,15 +1,40 @@
 import {describe, expect, it} from '@jest/globals';
 import {render, screen} from '@testing-library/react';
 import WritePage from "../../../src/routes/write/WritePage";
+import {userEvent} from "@testing-library/user-event";
 
 const testSource = "# Heading\n## Subheading";
 
 describe("Write Page", () => {
+
     it("Test render", async () => {
         // Render the Markdown component
+        const user = userEvent.setup();
         const {debug} = render(<WritePage/>);
+        const editor = screen.getByTestId("md-editor");
 
+        await user.type(editor, testSource);
+
+        const h1 = screen.getByText("Heading");
+        const h2 = screen.getByText("Subheading");
         // debug();
-        expect(true).toBe(false);
+        expect(h1.tagName).toBe("H1");
+        expect(h2.tagName).toBe("H2");
     });
+
+    it("Test upload & status", async () => {
+        const user = userEvent.setup();
+        const {debug} = render(<WritePage/>);
+        const editor = screen.getByTestId("md-editor");
+
+        // Test type status change
+        await user.type(editor, testSource);
+        const status = screen.getByText("Unsaved");
+        expect(status.textContent).toBe("Unsaved");
+
+        // Test submit
+        const submitBtn = screen.getByText("Submit");
+        await user.click(submitBtn);
+        expect(status.textContent).toBe("Saved!");
+    })
 })
