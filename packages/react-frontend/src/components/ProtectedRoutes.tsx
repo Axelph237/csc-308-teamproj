c3b1c673eeee491abcea78c02bb0943f5ed5b107
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-05-01 16:23:46 -0700

    Fixed ProtectedRoutes to correctly work as function component

--- packages/react-frontend/src/components/ProtectedRoutes.tsx
+++ packages/react-frontend/src/components/ProtectedRoutes.tsx
@@ -1,22 +1,41 @@
-import {Navigate, Outlet} from "react-router-dom";
-import {getUser} from "@src/api/backend";
+import {Navigate, Outlet, useNavigate} from "react-router-dom";
+import {getUser} from "../api/backend";
+import {useEffect, useState} from "react";
 
-export default async function ProtectedRoutes() {
+export default function ProtectedRoutes() {
+    let navigate = useNavigate();
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    let isAuthenticated: boolean;
-    try {
-        // Get truthy value of user type
-        // NOTE: This could be edited to instead set the user of the app.
-        isAuthenticated = !!(await getUser());
-    }
-    catch (e) {
-        console.log(e);
-        // Catch errors and force navigation
-        isAuthenticated = false;
-    }
+    // On component mount check auth
+    useEffect(() => {
+        const getAuth = async () => {
+            try {
+                // Get truthy value of user type
+                // NOTE: This could be edited to instead set the user of the app.
+                const user = await getUser();
+                console.log(user);
+                setIsAuthenticated(!!user);
+
+            }
+            catch (e) {
+                console.log(e);
+                // Catch errors and force navigation
+                setIsAuthenticated(false);
+            }
+        }
+        getAuth();
+    }, [])
+
+    // On auth update determine if navigate is necessary
+    useEffect(() => {
+        if (!isAuthenticated)
+            navigate("/login");
+    }, [isAuthenticated]);
 
     return ( isAuthenticated
             ? <Outlet />
-            : <Navigate to={'/login'}/>
+            : <div className="w-screen h-screen flex justify-center items-center">
+                <h1>Who invited this guy? 🫵😭</h1>
+            </div>
     )
 }
