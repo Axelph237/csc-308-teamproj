5f3efa063fd89ae7f5e1f676c5e7037ff0446b5f
gthac <gthacker@calpoly.edu>
2025-05-09 13:34:50 -0700

    changed some paths

--- packages/react-frontend/src/routes/landing/LandingPage.tsx
+++ packages/react-frontend/src/routes/landing/LandingPage.tsx
@@ -1,9 +1,4 @@
-/*
-in line - onClick={() => window.location.href='http://localhost:5173/login'}
-it would be more appropriate to change 'http://localhost:5173/login' to
-the current link with the addition of /login.
-This is true for each onClick() in this class.
- */
+import {Link} from "react-router-dom";
 
 export default function LandingPage() {
     return (
@@ -11,17 +6,18 @@ export default function LandingPage() {
             <h1 className="text-2xl font-bold ">Welcome to Diary</h1>
             <h2>In Diary you can store and create diary entries, just like any great diary!</h2>
 
-            <button
-                onClick={() => window.location.href='http://localhost:5173/login'}
-                className="flex p-4 rounded-2xl bg-accent-500 cursor-pointer hover:bg-accent-800">
-                Login
-            </button>
+            <div><Link to={'/home'} className="pl-6">
 
-            <button
-                onClick={() => window.location.href='http://localhost:5173/createProfile'}
-                className="flex p-4 rounded-2xl bg-accent-500 cursor-pointer hover:bg-accent-800">
-                Create Profile
-            </button>
+                <button className="flex p-4 rounded-2xl bg-accent-500 cursor-pointer hover:bg-accent-800">Login
+                </button>
+            </Link></div>
+
+            <div><Link to={'/createprofile'} className="pl-6">
+
+                <button className="flex p-4 rounded-2xl bg-accent-500 cursor-pointer hover:bg-accent-800">Create Profile
+                </button>
+            </Link></div>
+
         </div>
     );
 }
