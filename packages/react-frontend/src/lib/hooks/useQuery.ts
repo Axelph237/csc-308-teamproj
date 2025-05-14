6ec34dc455786a2be23a9451a91fb6d137afe06d
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-05-14 14:23:21 -0700

    Updated useQuery import

--- packages/react-frontend/src/lib/hooks/useQuery.ts
+++ packages/react-frontend/src/lib/hooks/useQuery.ts
@@ -1,7 +1,7 @@
 // A custom hook that builds on useLocation to parse
 // the query string for you.
 import {useMemo} from "react";
-import { useLocation } from 'react-router'
+import { useLocation } from 'react-router-dom'
 
 export default function useQuery() {
     const { search } = useLocation();
