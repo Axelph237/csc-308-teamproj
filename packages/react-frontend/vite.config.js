f9e8840a315c3647fa8204b27dfd4f33fac08bc6
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-05-16 12:13:38 -0700

    Moved domain specifications to config and CORS specs

--- packages/react-frontend/vite.config.js
+++ packages/react-frontend/vite.config.js
@@ -1,32 +1,36 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss()
-    ],
-    resolve: {
-        alias: {
-            '@src': resolve(dirname(fileURLToPath(import.meta.url)), 'src'),
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+
+    return defineConfig({
+        plugins: [
+            react(),
+            tailwindcss()
+        ],
+        resolve: {
+            alias: {
+                '@src': resolve(dirname(fileURLToPath(import.meta.url)), 'src'),
+            },
         },
-    },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8001',
-                changeOrigin: true,
-                secure: false,
-                cookieDomainRewrite: 'localhost',
-                rewrite: (path) => path.replace(/^\/api/, '')
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_TARGET_DOMAIN,
+                    changeOrigin: true,
+                    secure: false,
+                    cookieDomainRewrite: env.VITE_API_TARGET_DOMAIN,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                }
             }
-        }
-    },
-    build: {
-        outDir: 'dist',
-    },
-})
+        },
+        build: {
+            outDir: 'dist',
+        },
+    })
+}
