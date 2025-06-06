bcf5fd1eca5b9913460e3aa279718fd8d054acc7
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-06-06 02:11:43 -0700

    Updated some configs to load mode environ variables on build

--- packages/react-frontend/vite.config.js
+++ packages/react-frontend/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 
 // https://vite.dev/config/
-export default () => {
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
   return defineConfig({
     plugins: [react(), tailwindcss()],
@@ -16,6 +17,12 @@ export default () => {
     },
     build: {
       outDir: "dist",
+    },
+    define: {
+      __API_TARGET__: JSON.stringify(env.VITE_API_TARGET_DOMAIN)
+    },
+    server: {
+      historyApiFallback: true, // Add this
     },
   });
 };
