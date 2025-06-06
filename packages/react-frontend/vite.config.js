7091530ce17ba639a055ba336e9703b78280d609
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-06-06 01:47:44 -0700

    Reduced config

--- packages/react-frontend/vite.config.js
+++ packages/react-frontend/vite.config.js
@@ -1,29 +1,17 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 
 // https://vite.dev/config/
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+export default () => {
 
   return defineConfig({
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         "@src": resolve(dirname(fileURLToPath(import.meta.url)), "src"),
-      },
-    },
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_TARGET_DOMAIN,
-          changeOrigin: true,
-          secure: false,
-          cookieDomainRewrite: env.VITE_API_TARGET_DOMAIN,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
       },
     },
     build: {
